d4c629e3cd9d53339ff6a9b8cea1a1f3fdf355c4
缪宇 <miaoyu2009@qq.com>
2023-12-01 17:02:05 +0800

    feat: 适配 h5

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts"></script>
 <template>
-  <div :class="$style.container">home</div>
+  <div :class="$style.container">
+    <div :class="$style.box"></div>
+    home
+  </div>
 </template>
 <style module lang="less">
 .container {
+}
+.box {
+  width: 750px;
+  height: 20px;
+  background-color: red;
 }
 </style>
