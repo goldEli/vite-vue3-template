48e1ecc9fe72bf746b7e298cfe8abd973443f45d
缪宇 <miaoyu2009@qq.com>
2023-12-01 17:33:51 +0800

    feat: modify port 5000

--- vite.config.ts
+++ vite.config.ts
@@ -49,7 +49,7 @@ export default defineConfig(({ mode }) => {
       // 是否开启 https
       https: false,
       // 端口号
-      port: 3000,
+      port: 5000,
       // 监听所有地址
       host: "0.0.0.0",
       // 服务启动时是否自动打开浏览器
