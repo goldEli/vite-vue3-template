fcbf7caf3949462440b2f20a933eabeece67b77d
缪宇 <miaoyu2009@qq.com>
2023-12-14 14:36:37 +0800

    feat: basic info

--- src/views/addStaff/BasicInfo.vue
+++ src/views/addStaff/BasicInfo.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const name = ref("");
+const name = ref("张三");
 const phone = ref("");
 const job = ref("");
 const department = ref("");
@@ -10,11 +10,11 @@ const englishName = ref("");
 </script>
 <template>
   <van-cell-group inset>
-    <van-field v-model="name" name="name" label="姓名" />
+    <van-field disabled v-model="name" name="name" label="姓名" />
     <van-field v-model="phone" name="phone" label="手机号" />
     <van-field v-model="job" name="job" label="岗位" />
     <van-field v-model="department" name="department" label="部门" />
-    <van-field name="gender" label="性别">
+    <van-field name="gender" label="性别" :rules="[{ required: true, message: '请选择性别' }]">
       <template #input>
         <van-radio-group v-model="gender" direction="horizontal">
           <van-radio name="1">男</van-radio>
